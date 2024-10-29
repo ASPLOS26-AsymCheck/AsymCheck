18b682a8ce351319991efb8b6a7d87499a955dcd
EuroSys25-OTeaCheck <m16602706192@163.com>
2024-10-29 12:19:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='deepspeed_lib',
+setup(name='oteacheck_lib',
       version='1.0',
       description='Reconstructing the DeepSpeed training framework',
       packages=find_packages()
