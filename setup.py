9aa737300fd42e3f73ec84266cd45c012e239f5e
FAST26-DelayCheck <m16602706192@163.com>
2025-03-19 22:39:46 +0800

    Update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='oteacheck_lib',
+setup(name='delaycheck_lib',
       version='1.0',
       description='Reconstructing the DeepSpeed training framework',
       packages=find_packages()
